f61a03159d38db0ded47c93d58fcd71a72cc6ac9
wilson <l7960261@gmail.com>
2019-09-17 16:20:24 +0800

    Add angularjs-jwt for test

--- src/app.module.js
+++ src/app.module.js
@@ -1,4 +1,13 @@
 import angular from 'angular';
+import jwtModule from 'angularjs-jwt';
 
 angular
-.module('spa-module-ng1', [])
+  .module('spa-module-ng1', [jwtModule.name])
+  .config(['jwtAuthenticationProvider', function (jwtAuthenticationProvider) {
+    jwtAuthenticationProvider.changeOptions({
+      refreshTokenURI: 'http://localhost:8080/api/authRefresh',
+    });
+  }])
+  .config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('jwtInterceptor');
+  }])
--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,23 @@
 angular
-.module('spa-module-ng1')
-.component('rootNg1', {
-  template: '<h1>This is AngularJS project</h1>'
-});
+  .module('spa-module-ng1')
+  .component('rootNg1', {
+    template: `
+    <h1>This is AngularJS project</h1>
+    <button type="button" ng-click="$ctrl.test()">Fetch 10 request</button>
+  `,
+    controller: RootCtrl
+  });
+
+function RootCtrl($http) {
+
+  this.test = function () {
+    for (let index = 0; index < 10; index++) {
+      console.log('Fetch getData - ' + index);
+      $http.post('http://localhost:8080/api/getData', { times: index })
+        .then(arg => console.log(arg));
+    }
+  };
+
+}
+
+RootCtrl.$inject = ['$http'];
